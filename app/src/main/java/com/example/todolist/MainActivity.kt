b6d4db1793accb2d1868b7d04574e4aaa17c5a30
Taysmar <taysmar@outlook.com>
2021-09-06 21:16:06 -0300

    Adição de telas

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater())
+        setContentView(binding.root)
+
+        insertListeners()
+
     }
+
+    private fun insertListeners() {
+    binding.fab.setOnClickListener{
+        startActivity(Intent(this, AddTaskActivity::class.java))
+    }
+        }
+
 }
